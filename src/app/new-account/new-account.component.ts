4b00aa0e8b0c233a6460e85d8695146eb29070a9
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-09-27 12:40:35 +0530

    Commit#14

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -10,7 +10,7 @@ import { LoggingService } from '../logging.service';
 export class NewAccountComponent {
   @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
 
-  constructor(private loggingService: LoggingService)
+  constructor(private loggingService: LoggingService){}
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountAdded.emit({
