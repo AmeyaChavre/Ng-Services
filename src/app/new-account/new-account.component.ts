81518c32f4d8c6f7fcaec6d223b49543396fc856
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-09-28 10:52:42 +0530

    Commit#43

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -14,11 +14,6 @@ export class NewAccountComponent {
   constructor(private loggingService: LoggingService){}
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
-    //console.log('A server status changed, new status: ' + accountStatus);
     this.loggingService.logStatusChange(accountStatus);
   }
 }
