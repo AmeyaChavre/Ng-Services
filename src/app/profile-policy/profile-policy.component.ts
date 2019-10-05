58642427370dc10cac3afbe1d779f97c0d5dcb93
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-10-05 13:33:20 +0530

    Commit#72

---
+++ src/app/profile-policy/profile-policy.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-policy',
+  templateUrl: './profile-policy.component.html',
+  styleUrls: ['./profile-policy.component.css']
+})
+export class ProfilePolicyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
